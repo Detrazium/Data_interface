6b3ff2870a32b0fb0a07e15e93f460b132ada20b
Антон <lazutckin.anton@yandex.ru>
2024-05-08 20:07:37 +0300

    commit #6

--- Test.py
+++ Test.py
@@ -7,6 +7,7 @@ QMainWindow,
 QTableWidget,
 QLineEdit,
 QLabel,
+QMessageBox,
 
 QVBoxLayout,
 QHBoxLayout,
@@ -17,27 +18,33 @@ class Face(QWidget):
 	def __init__(self):
 		super().__init__()
 		self.setFixedSize(320, 200)
-		# self.setGeometry(50, 100, 300, 100)
 		self.setWindowTitle('Приложение')
 
 		self.kc = QHBoxLayout()
 		self.AUt = LogotBOX(self)
-		self.btn = QPushButton('Существующие\n логины и пароли')
+		self.btn = QPushButton('_проверка_\nСуществующие\n логины и пароли')
+		self.btn.clicked.connect(self.Log_test)
 
 		self.kc.addWidget(self.AUt)
 		self.kc.addWidget(self.btn)
 		self.setLayout(self.kc)
 
-
+	def Log_test(self):
+		self.mess = QMessageBox()
+		self.mess.setWindowTitle('Выберите проверяемое')
+		self.mess.setText(' Проверяемое | Существующие логины и пароли внутри отдельного файла CSV. ')
+		with open('CSVs/csv_user.csv', 'r', encoding='utf=8') as csvfile:
+			file = csvfile.read().replace('\n', '\n\n').replace(',', ' | ')
+			self.mess.setDetailedText(file)
+		self.mess.show()
 class LogotBOX(QWidget):
 	def __init__(self, wg):
 		self.wg = wg
 		super().__init__(wg)
-		self.setGeometry(50, 50, 150, 300)
-
 		self.laye = QVBoxLayout()
 
 		self.bnn = QPushButton("Авторизироваться")
+		self.bnn.clicked.connect(self.connecte)
 
 		self.occ_ = QLabel('Login')
 		self.occ = QLineEdit(self)
@@ -52,8 +59,43 @@ class LogotBOX(QWidget):
 		self.laye.addWidget(self.occ2)
 		self.laye.addWidget(self.bnn)
 		self.setLayout(self.laye)
+	def connecte(self):
+		print('Autorising...')
+		self.wg.close()
+
+		self.user = user_window()
+		self.user.show()
+class user_window(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.setFixedSize(500, 500)
+		self.cont = QVBoxLayout()
 
 
+
+
+
+		self.inn = windowInfo(self)
+		self.cont.addWidget(self.inn)
+		self.setLayout(self.cont)
+class windowInfo(QWidget):
+	def __init__(self,wg):
+		self.wg = wg
+		super().__init__(wg)
+		self.laye = QHBoxLayout()
+
+		self.btn = QPushButton('exit')
+		self.btn.clicked.connect(self.closed)
+
+
+
+
+
+		self.laye.addWidget(self.btn)
+		self.setLayout(self.laye)
+	def closed(self):
+		print('close')
+		self.wg.close()
 ppp = QApplication(sys.argv)
 exe = Face()
 exe.show()
